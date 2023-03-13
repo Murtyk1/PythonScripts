75f13e4f07222cad0f1f9aa50ddffd14e3773094
murat <muratik_kishmakhov@mail.ru>
2023-03-13 23:03:58 +0300

    asdfsg

--- venv/main.py
+++ venv/main.py
@@ -15,4 +15,4 @@ for i in range(len(sides)):
                 if s > smax:
                     smax = s
 
-print("Максимальная площадь треугольника", smax)
+print("Максимальная площадь ника", smax)
